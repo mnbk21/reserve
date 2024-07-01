de9ab90994fff648ca7f6a6645dc3a22f0ef4e4b
mnbk21 <mnbk21@gmail.com>
2024-07-01 22:34:24 +0900

    予約内容確認画面

--- web/confirm.php
+++ web/confirm.php
@@ -1,3 +1,9 @@
+<?php
+require_once 'functions.php';
+
+session_start();
+
+?>
 <!doctype html>
 <html lang="ja">
 
@@ -5,43 +11,44 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
     <!-- Original CSS -->
     <link href="/css/style.css" rel="stylesheet">
+
     <title>予約内容確認</title>
   </head>
 
   <body>
     <header>SAMPLE SHOP</header>
     <h1>予約内容確認</h1>
-
     <table class="table">
       <tbody>
         <tr>
           <th scope="row">日時</th>
-          <td>2022年01月01日（土）17時00分</td>
+          <td><?= format_date($_SESSION['RESERVE']['reserve_date']) ?> <?= $_SESSION['RESERVE']['reserve_time'] ?></td>
         </tr>
         <tr>
           <th scope="row">人数</th>
-          <td>4名</td>
+          <td><?= $_SESSION['RESERVE']['reserve_num'] ?>名</td>
         </tr>
         <tr>
           <th scope="row">氏名</th>
-          <td colspan="2">XXXXXX</td>
+          <td colspan="2"><?= $_SESSION['RESERVE']['name'] ?></td>
         </tr>
         <tr>
           <th scope="row">メールアドレス</th>
-          <td colspan="2">XXX@XXX.xxx</td>
+          <td colspan="2"><?= $_SESSION['RESERVE']['email'] ?></td>
         </tr>
         <tr>
           <th scope="row">電話番号</th>
-          <td colspan="2">111-111-1111</td>
+          <td colspan="2"><?= $_SESSION['RESERVE']['tel'] ?></td>
         </tr>
         <tr>
           <th scope="row">備考</th>
-          <td colspan="2">XXXXXXXXXXXXXXXXXXXXXX<br>
-            XXXXXXXXXXXXXXXXXXXXXX</td>
+          <td colspan="2"><?= nl2br($_SESSION['RESERVE']['comment']) ?></td>
         </tr>
       </tbody>
     </table>
@@ -52,6 +59,7 @@
     </div>
 
     <!-- Optional JavaScript; choose one of the two! -->
+
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
--- web/functions.php
+++ web/functions.php
@@ -1,13 +1,15 @@
 <?php
+// 引数で与えられた配列を元にプルダウンリストを生成する
 function arrayToSelect($inputName, $srcArray, $selectedIndex = "")
 {
   $temphtml = "<select class=\"form-select\" name=\"{$inputName}\">" . PHP_EOL;
 
   foreach ($srcArray as $key => $val) {
+    // キーと選択値を比較して一致したらselectedを付ける
     if ($key == $selectedIndex) {
-        $selectedText = " selected";
+      $selectedText = " selected";
     } else {
-        $selectedText = "";
+      $selectedText = "";
     }
     $temphtml .= "<option value=\"{$key}\"{$selectedText}>{$val}</option>" . PHP_EOL;
   }
@@ -15,4 +17,11 @@ function arrayToSelect($inputName, $srcArray, $selectedIndex = "")
   $temphtml .= "</select>" . PHP_EOL;
 
   return $temphtml;
+}
+
+// 引数で与えられた日付を表示形式に変換する
+function format_date($yyyymmdd)
+{
+  $week = array('日', '月', '火', '水', '木', '金', '土');
+  return date('n/j(' . $week[date('w', strtotime($yyyymmdd))] . ')', strtotime($yyyymmdd));
 }
